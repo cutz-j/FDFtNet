b71ca554a2871f925f8cacb24da4da05bb0d3f8a
cutz-j <cutz-j@naver.com>
2020-09-21 17:04:53 +0900

    Update
    Update

--- fdft.py
+++ fdft.py
@@ -10,81 +10,95 @@ from keras.layers import Input, Dense, Flatten, GlobalAveragePooling2D, Activati
 from keras.layers import SeparableConv2D, Add, Convolution2D, concatenate, Layer, ReLU, DepthwiseConv2D, Reshape, Multiply, InputSpec
 from keras.regularizers import l2
 from keras.models import Model
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 
-"""
-ft_dir
-shear_range
-zoom_range
-rotation_range
-width_shift_range
-height_shift_range
-horizontal_flip
-zca_whitening
-cutout_use (boolean)
-if cutout_use:
-    co = cutout
-else:
-    co = None
+# argparse
+parser = argparse.ArgumentParser(description='Pretrain the models')
 
-img_height
-img_width
-batch_size
-validation_dir
-ld
-"""
-img_height = 64
-img_width = 64
+parser.add_argument('-pt_model', required=True, type=str, help='select the pre-trained network')
+parser.add_argument('-network', required=True, type=str, help='select the backbone network')
+parser.add_argument('-ft_dir', required=True, type=str, help='train image directory')
+parser.add_argument('-val_dir', required=True, type=str, help='validation image directory')
+parser.add_argument('-img_height', type=str, default=64, help='image height')
+parser.add_argument('-img_width', type=int, default=64, help='image width')
+parser.add_argument('-batch_size', type=int, default=128, help='batch_size')
+parser.add_argument('-es_patience', type=int, default=20, help='early stopping patience')
+parser.add_argument('-reduce_factor', type=int, default=0.1, help='reduce factor')
+parser.add_argument('-reduce_patience', default=20, type=int, help='reduce patience')
+parser.add_argument('-step', type=int, default=200, help='steps per epoch')
+parser.add_argument('-epochs', type=int, default=300, help='epochs')
+parser.add_argument('-dropout_rate', type=int, default=0.2, help='dropout rate')
+parser.add_argument('-gpu_ids', type=str, default='0', help='select the GPU to use')
 
-os.environ["CUDA_VISIBLE_DEVICES"] = '0'
-### 나중에 지우기
+# augmentation
+parser.add_argument('-shear_range', type=float, default=0, help='shear')
+parser.add_argument('-zoom_range', type=float, default=0, help='zoom')
+parser.add_argument('-rotation_range', type=float, default=0.2, help='rotation')
+parser.add_argument('-width_shift_range', type=float, default=2.0, help='width shift')
+parser.add_argument('-height_shift_range', type=float, default=2.0, help='height shift')
+parser.add_argument('-horizontal_flip', type=bool, default=True, help='horizontal flip')
+parser.add_argument('-zca_whitening', type=bool, default=False, help='zca_whitening')
+parser.add_argument('-cutout_use', type=bool, default=True, help='zca_whitening')
 
+
+args = parser.parse_args()
+
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_ids
+
+# model selection
+if args.network.lower() == 'xception':
+    base_model = xception(args.img_height, args.img_weight, args.dropout_rate)
+elif args.network.lower() == 'resnetv2':
+    base_model = resNetV2(args.img_height, args.img_weight)
+elif args.network.lower() == 'squeezenet':
+    base_model = squeezeNet(args.img_height, args.img_weight, args.dropout_rate)
 
 
 ##
+if args.cutout_use == False:
+    cutout = None
 
-ft_dir = '/mnt/a/fakedata/deepfake/finetune'
-train_gen_aug = ImageDataGenerator(shear_range=0,
-                               zoom_range=0,
-                               rotation_range=0.2,
-                               width_shift_range=2.,
-                               height_shift_range=2.,
-                               horizontal_flip=True,
-                               zca_whitening=False,
+ft_dir = args.ft_dir
+train_gen_aug = ImageDataGenerator(shear_range=args.shear_range,
+                               zoom_range=args.zoom_range,
+                               rotation_range=args.rotation_range,
+                               width_shift_range=args.width_shift_range,
+                               height_shift_range=args.height_shift_range,
+                               horizontal_flip=args.horizontal_flip,
+                               zca_whitening=args.zca_whitening,
                                fill_mode='nearest',
                                preprocessing_function=cutout) # chagne this to co
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 ft_gen = train_gen_aug.flow_from_directory(ft_dir,
-                                              target_size=(img_height, img_width),
-                                              batch_size=batch_size,
+                                              target_size=(args.img_height, args.img_width),
+                                              batch_size=args.batch_size,
                                               shuffle=True,
                                               class_mode='categorical')
 
 
-validation_generator = test_datagen.flow_from_directory(validation_dir,
-                                                        target_size=(img_height, img_width),
-                                                        batch_size=batch_size,
+validation_generator = test_datagen.flow_from_directory(args.val_dir,
+                                                        target_size=(args.img_height, args.img_width),
+                                                        batch_size=args.batch_size,
                                                         shuffle=False,
                                                         class_mode='categorical')
 
 
-model_ft = load_model('/home/www/fake_detection/model/deepfake_xception.h5') # change this to weight
+model_ft = load_model(args.pt_model) # change this to weight
 for i in range(2):
     model_ft.layers.pop()
-im_in = Input(shape=(img_width, img_height, 3))
-
-if --#xception, resnet,squeezenet
-    base_model = xception(include_top=False)
-    base_model.set_weights(model_ft.get_weights())
+im_in = Input(shape=(args.img_width, args.img_height, 3))
+base_model.set_weights(model_ft.get_weights())
 # for i in range(len(base_model.layers) - 0):
 #     base_model.layers[i].trainable = False
 
 pt_output = base_model(im_in)
 
 mb = block(shape=tf.Tensor.get_shape(pt_output)[1:])
-ftt = att(shape=(img_width, img_height, 3))
+ftt = att(shape=(args.img_width, args.img_height, 3))
 
 mb_output = mb(pt_output)
 ftt_output = ftt(im_in)
@@ -100,7 +114,7 @@ model_top.summary()
 # optimizer = SGD(lr=1e-3, momentum=0.9, nesterov=True)
 optimizer = Adam()
 model_top.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['acc'])
-callback_list = [EarlyStopping(monitor='val_acc', patience=es_patieance),
-                 ReduceLROnPlateau(monitor='loss', factor=reduce_factor, cooldown=0, patience=5, min_lr=0.5e-5)]
-output = model_top.fit_generator(ft_gen, steps_per_epoch=200, epochs=300,
+callback_list = [EarlyStopping(monitor='val_accuracy', patience=args.es_patience),
+                 ReduceLROnPlateau(monitor='val_loss', factor=args.reduce_factor, patience=args.reduce_patience)]
+output = model_top.fit_generator(ft_gen, steps_per_epoch=args.step, epochs=args.epochs,
                                   validation_data=validation_generator, validation_steps=len(validation_generator), callbacks=callback_list)
--- pretrain.py
+++ pretrain.py
@@ -13,15 +13,11 @@ parser.add_argument('-train_dir', required=True, type=str, help='train image dir
 parser.add_argument('-val_dir', required=True, type=str, help='validation image directory')
 parser.add_argument('-img_height', type=str, default=64, help='image height')
 parser.add_argument('-img_width', type=int, default=64, help='image width')
-parser.add_argument('-batch_size', required=True, type=int, help='batch_size')
-<<<<<<< HEAD
+parser.add_argument('-batch_size', type=int, default=128, help='batch_size')
 parser.add_argument('-es_patience', type=int, default=20, help='early stopping patience')
-=======
-parser.add_argument('-es_patience', type=int, default=20, help='early stopping patience') 
->>>>>>> 135c7b1f8471f21061fb72a06e3b225fe0d84589
-parser.add_argument('-reduce_factor', required=True, type=int, help='reduce factor')
-parser.add_argument('-reduce_patience', required=True, type=int, help='reduce patience')
-parser.add_argument('-step', required=True, type=int, help='steps per epoch')
+parser.add_argument('-reduce_factor', type=int, default=0.1, help='reduce factor')
+parser.add_argument('-reduce_patience', default=20, type=int, help='reduce patience')
+parser.add_argument('-step', type=int, default=200, help='steps per epoch')
 parser.add_argument('-epochs', type=int, default=300, help='epochs')
 parser.add_argument('-dropout_rate', type=int, default=0.2, help='dropout rate')
 parser.add_argument('-gpu_ids', type=str, default='0', help='select the GPU to use')
@@ -34,11 +30,11 @@ weight_save_dir = os.path.join(root_dir, 'weights')
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_ids
 
 # model selection
-if args.network == 'xception':
+if args.network.lower() == 'xception':
     model = xception(args.img_height, args.img_weight, args.dropout_rate)
-elif args.network == 'resnetV2':
+elif args.network.lower() == 'resnetv2':
     model = resNetV2(args.img_height, args.img_weight)
-elif args.network == 'squeezenet':
+elif args.network.lower() == 'squeezenet':
     model = squeezeNet(args.img_height, args.img_weight, args.dropout_rate)
 
 
